c77f991f63d993834694806cba1a71406745da07
Albert Arrieta <albert.arrieta@uleth.ca>
2021-11-24 14:04:41 -0700

    Repository setup + Node.h setup

---
+++ include/Node.h
@@ -0,0 +1,66 @@
+#ifndef NODE_H
+#define NODE_H
+#include <string>
+#include <iostream>
+
+class Node {
+ public:
+
+   /**
+   *  Constructor for Node. Has 1 parameter for the value of the node.
+   */
+    Node(int v);
+
+    /**
+    * Desctructor for Node.
+    */
+    virtual ~Node();
+
+    /**
+    * Sets Node value to the integer argument passed.
+    */
+    void setValue(int v);
+
+    /**
+    * Sets Node height to the integer argument passed.
+    */
+    void setHeight(int h);
+
+    /**
+    * Sets leftNode to the Node argument passed.
+    */
+    void setLeftNode(Node* ln);
+
+    /**
+    * Sets rightNode to the node argument passed.
+    */
+    void setRightNode(Node* rn);
+
+    /**
+    * Returns value.
+    */
+    int getValue();
+
+    /**
+    * Returns Height.
+    */
+    int getHeight();
+
+    /**
+    * Returns the Left Node.
+    */
+    int getLeftNode();
+
+    /**
+    * Returns the Right Node.
+    */
+    int getRightNode();
+
+ private:
+   int value;
+   int height;
+   Node* leftNode;
+   Node* rightNode;
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Node.h"
+#include "AVLTree.h"
+#include <string>
+using namespace std;
+int main() {
+  cout << "HELLO WORLD" << endl;
+}
