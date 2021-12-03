356d0ed1295764afd0d6decae8872fdeb3d5b1a3
Albert Arrieta <albert.arrieta@uleth.ca>
2021-12-03 14:16:49 -0700

    INSERTION WORKS!!!

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,33 @@ struct Node {
     //change height
     visitedNode->height = 1 + max(visitedNode->left->getHeight(), visitedNode->right->getHeight());
 
+    //balance
+    int balanceFactor;
+    if (visitedNode == NULL) {
+      balanceFactor = 0;
+    } else {
+      cout << "Left height: " << visitedNode->left->getHeight() << endl;
+      cout << "Right height: " << visitedNode->right->getHeight() << endl;
+      balanceFactor = visitedNode->left->getHeight() - visitedNode->right->getHeight();
+    }
+
+    //rotations
+    if (balanceFactor > 1 && visitedNode->left->value > val) {
+      cout << "right rotation" << endl;
+      return rightRotation(visitedNode);
+    } else if (balanceFactor < -1 && visitedNode->right->value < val) {
+      cout << "left rotation" << "Balance factor = " << balanceFactor << endl;
+      return leftRotation(visitedNode);
+    } else if (balanceFactor > 1 && visitedNode->left->value < val) {
+      cout << "left right rotation" << endl;
+      visitedNode->left = leftRotation(visitedNode->left);
+      return rightRotation(visitedNode);
+    } else if (balanceFactor < -1 && visitedNode->right->value > val) {
+      cout << "right left rotation" << endl;
+      visitedNode->right = rightRotation(visitedNode->right);
+      return leftRotation(visitedNode);
+    }
+
     return visitedNode;
   }
 
@@ -55,15 +82,66 @@ struct Node {
     }
   }
 
+  Node* leftRotation(Node* n) {
+    Node* tempNode1 = n->right;
+    Node* tempNode2 = tempNode1->left;
+
+    //rotation
+    tempNode1->left = n;
+    n->right = tempNode2;
+    cout << "LEFT ROTATED!!" << endl;
+    cout << "Left of t1: " << tempNode1->left->value <<endl;
+    cout << "Right of t1: " << tempNode1->right->value <<endl;
+    //change heights
+    Node* nLeftNode = n->left;
+    Node* nRightNode = n->right;
+    Node* tn1LeftNode = tempNode1->left;
+    Node* tn1RightNode = tempNode1->right;
+
+    n->height = max(nLeftNode->getHeight(), nRightNode->getHeight()) + 1;
+    tempNode1->height = max(tn1LeftNode->getHeight(), tn1RightNode->getHeight()) + 1;
+
+    return tempNode1;
+  }
+
+  Node* rightRotation(Node* n) {
+    Node* tempNode1 = n->left;
+    Node* tempNode2 = tempNode1->right;
+
+    //rotation
+    tempNode1->right = n;
+    n->left = tempNode2;
+
+    //change heights
+    Node* nLeftNode = n->left;
+    Node* nRightNode = n->right;
+    Node* tn1LeftNode = tempNode1->left;
+    Node* tn1RightNode = tempNode1->right;
+
+    n->height = max(nLeftNode->getHeight(), nRightNode->getHeight()) + 1;
+    tempNode1->height = max(tn1LeftNode->getHeight(), tn1RightNode->getHeight()) + 1;
+
+    return tempNode1;
+  }
+
 };
 
 int main() {
   struct Node* root = new Node(6);
 
-  root->insert(root, 10);
-  root->insert(root, 20);
+  root = root->insert(root, 10);
+  root = root->insert(root, 20);
+  root = root->insert(root, 30);
+  root = root->insert(root, 5);
+  root = root->insert(root, 2);
+  root = root->insert(root, 3);
+  root = root->insert(root, 4);
+  root = root->insert(root, 1);
 
+
+  cout << "=========================" << endl;
   cout << "Root: " << root->value << " Height: " << root->height << endl;
-  cout << "right: " << root->right->value << " Height: " << root->right->height << endl;
-  cout << "rightright: " << root->right->right->value  << " Height: " << root->right->right->height << endl;
+  cout << "Right: " << root->right->value << endl;
+  cout << "Left: " << root->left->value << endl;
+  cout << "LeftLeft: " << root->right->right->value << endl;
 }
